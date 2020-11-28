e8e344972d75d180547cce31b2948e827843b866
manzar96 <manoszar96@gmail.com>
2020-11-28 18:34:02 +0200

    main for bert task71

--- experiments/train_bert.py
+++ experiments/train_bert.py
@@ -1,10 +1,15 @@
 import math
 import torch
+import torch.nn as nn
 from tqdm import tqdm
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer, BertModel
-from core.models.modules.heads import ClassificationHead
+
+from core.data.dataset import Task71Dataset
+from core.data.collators import Task71aCollator
+from core.models.modules.heads import BertClassificationHead
+from core.trainers import BertTrainer
 from core.utils.parser import get_train_parser
 
 
@@ -23,6 +28,19 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 # SEP token will work as EOS token
 # tokenizer.eos_token = tokenizer.sep_token
 
+# load dataset
+dataset = Task71Dataset("train", tokenizer=tokenizer)
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [7200,
+                                                                     800],
+                              generator=torch.Generator().manual_seed(42))
+
+collator_fn = Task71aCollator(device='cpu')
+train_loader = DataLoader(train_dataset, batch_size=options.batch_size,
+                          drop_last=False, shuffle=True,
+                          collate_fn=collator_fn)
+val_loader = DataLoader(val_dataset, batch_size=options.batch_size,
+                        drop_last=False, shuffle=True,
+                        collate_fn=collator_fn)
 
 # create model
 if options.modelckpt is not None:
@@ -32,7 +50,7 @@ else:
 
 # change config if you want
 # model.config.output_hidden_states = True
-model = ClassificationHead(model, model.config.hidden_size, num_classes=2,
+model = BertClassificationHead(model, model.config.hidden_size, num_classes=2,
                            drop=0.2,
                            act='sigmoid')
 
@@ -47,13 +65,15 @@ print('Trainable Parameters: {}'.format(train_numparams))
 optimizer = Adam(
     [p for p in model.parameters() if p.requires_grad],
     lr=options.lr, weight_decay=1e-6)
-
-
+criterion = nn.CrossEntropyLoss(ignore_index=-100)
+metrics = ['f1-score']
 import ipdb;ipdb.set_trace()
 
-# # create trainer
-# trainer = BertTrainer(model=model, optimizer=optimizer,
-#                                  patience=5, scheduler=None,
-#                                  checkpoint_dir=options.ckpt, device=DEVICE)
-# # train model
-# trainer.fit(train_loader, val_loader, epochs=options.epochs)
+# create trainer
+
+trainer = BertTrainer(model=model, optimizer=optimizer, criterion=criterion,
+                      metrics=metrics,
+                      patience=5, scheduler=None,
+                      checkpoint_dir=options.ckpt, device=DEVICE)
+# train model
+trainer.fit(train_loader, val_loader, epochs=options.epochs)
