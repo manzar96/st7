78d18fbe74adfcbe9f7ed7af6026eb900d343825
manzar96 <manoszar96@gmail.com>
2020-11-30 00:53:38 +0200

    added simple clf head

--- core/models/modules/heads.py
+++ core/models/modules/heads.py
@@ -1,5 +1,24 @@
 import torch.nn as nn
 
+class ClassificationHead(nn.Module):
+    def __init__(self, encoder, encoded_features, num_classes,drop=0,
+                 act='none'):
+        super(ClassificationHead, self).__init__()
+        self.encoder = encoder
+        self.clf = nn.Linear(encoded_features, num_classes)
+        self.drop = nn.Dropout(drop)
+        if act is 'none':
+            self.act = None
+        elif act is 'sigmoid':
+            self.act = nn.Sigmoid()
+
+    def forward(self, *args, **kwargs):
+        outputs = self.encoder(*args, **kwargs)
+        out = self.clf(outputs)
+        if self.act:
+            out = self.act(out)
+        out = self.drop(out)
+        return out
 
 class BertClassificationHead(nn.Module):
     def __init__(self, encoder, encoded_features, num_classes,drop=0,
