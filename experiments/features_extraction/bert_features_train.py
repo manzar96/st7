098108c65353bd2f38a384b5794b0b66a4e2ccc1
manzar96 <manoszar96@gmail.com>
2020-11-30 19:46:41 +0200

    implemented bert feature extraction for train samples

--- experiments/features_extraction/bert_features_train.py
+++ experiments/features_extraction/bert_features_train.py
@@ -74,6 +74,6 @@ if options.modelckpt is not None:
 model.to(DEVICE)
 
 res_dict = get_features(loader, model, DEVICE)
-if not os.path.exists('./features/'):
-    os.makedirs('./features')
-pickle.dump(res_dict, open("./features/bert_features.pkl", "wb"))
+if not os.path.exists('./features_train/'):
+    os.makedirs('./features_train')
+pickle.dump(res_dict, open("./features_train/bert_features.pkl", "wb"))
