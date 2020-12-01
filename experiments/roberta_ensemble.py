cbc19d2758f659f0e533de76d3e6eb3ef3f687d0
manzar96 <manoszar96@gmail.com>
2020-12-01 02:05:53 +0200

    debugging

--- experiments/roberta_ensemble.py
+++ experiments/roberta_ensemble.py
@@ -43,8 +43,8 @@ encoder = RobertaModel.from_pretrained('roberta-base')
 base_estimator = BertClassificationHead(encoder, encoder.config.hidden_size,
                                         num_classes=2, drop=0.2)
 
-model = VotingClassifier(estimator=base_estimator, n_estimators=3,
-                         output_dim=2,lr=1.5e-5,epochs=10,weight_decay=1e-6)
+model = VotingClassifier(estimator=base_estimator, n_estimators=2,
+                         output_dim=2,lr=1.5e-5,epochs=1,weight_decay=1e-6)
 import ipdb;ipdb.set_trace()
 model.fit(train_loader)
 import ipdb;ipdb.set_trace()
