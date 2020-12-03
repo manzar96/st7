c99e6f592c76fd04d7785a26cd34e21764480806
manzar96 <manoszar96@gmail.com>
2020-12-03 05:03:07 +0200

    debugging

--- experiments/task73/train_bert.py
+++ experiments/task73/train_bert.py
@@ -47,7 +47,7 @@ encoder = BertModel.from_pretrained('bert-base-uncased')
 # encoder.config.output_hidden_states = True
 model = BertClassificationHead(encoder, encoder.config.hidden_size,
                                act='sigmoid',
-                               num_classes=1, drop=0.2)
+                               num_classes=2, drop=0.2)
 if options.modelckpt is not None:
     state_dict = torch.load(options.modelckpt,map_location='cpu')
     model.load_state_dict(state_dict)
