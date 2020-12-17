7034a854a04f5b9d9328dfb38352e64e31c8f66c
manzar96 <manoszar96@gmail.com>
2020-12-17 02:06:39 +0200

    load pretrained bert from negative sampling

--- experiments/task71/train_bert.py
+++ experiments/task71/train_bert.py
@@ -43,15 +43,17 @@ val_loader = DataLoader(val_dataset, batch_size=options.batch_size,
                         collate_fn=collator_fn)
 
 # create model
-encoder = BertModel.from_pretrained('bert-base-uncased')
-
-# change config if you want
-# encoder.config.output_hidden_states = True
-model = BertClassificationHead(encoder, encoder.config.hidden_size,
-                               num_classes=2, drop=0.2)
+model = BertModel.from_pretrained('bert-base-uncased')
 if options.modelckpt is not None:
     state_dict = torch.load(options.modelckpt,map_location='cpu')
     model.load_state_dict(state_dict)
+# change config if you want
+# encoder.config.output_hidden_states = True
+model = BertClassificationHead(model, model.config.hidden_size,
+                               num_classes=2, drop=0.2)
+# if options.modelckpt is not None:
+#     state_dict = torch.load(options.modelckpt,map_location='cpu')
+#     model.load_state_dict(state_dict)
 
 model.to(DEVICE)
 
