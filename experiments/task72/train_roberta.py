dd78c6db1a161bec92c5116aee16894a22a2a481
manzar96 <manoszar96@gmail.com>
2021-01-07 16:29:02 +0200

    roberta rgr

--- experiments/task72/train_roberta.py
+++ experiments/task72/train_roberta.py
@@ -44,9 +44,9 @@ val_loader = DataLoader(val_dataset, batch_size=options.batch_size,
 encoder = RobertaModel.from_pretrained('roberta-base')
 
 # change config if you want
-# encoder.config.output_hidden_states = True
+encoder.config.output_hidden_states = True
 model = BertClassificationHead(encoder, encoder.config.hidden_size,act='relu',
-                               num_classes=1, drop=0.2)
+                               num_classes=1, drop=0.2, method=options.method)
 if options.modelckpt is not None:
     state_dict = torch.load(options.modelckpt,map_location='cpu')
     model.load_state_dict(state_dict)
