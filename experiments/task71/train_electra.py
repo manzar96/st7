ec344fe403297c6fbbcaa7795255f7b1a67bf6ce
manzar96 <manoszar96@gmail.com>
2021-01-12 03:12:10 +0200

    updated main for electra

--- experiments/task71/train_electra.py
+++ experiments/task71/train_electra.py
@@ -74,8 +74,8 @@ import ipdb;ipdb.set_trace()
 
 trainer = BertTrainer(model=model, optimizer=optimizer, criterion=criterion,
                       metrics=metrics,
-                      checkpoint_max=False,
-                      checkpoint_with=None,
+                      checkpoint_max=True,
+                      checkpoint_with='f1-score',
                       patience=5, scheduler=None,
                       checkpoint_dir=options.ckpt, device=DEVICE)
 # train model
